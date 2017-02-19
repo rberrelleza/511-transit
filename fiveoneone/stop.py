2d3fc26572b185c03d00f3270473402fffc25397
Ramiro Berrelleza <rberrelleza@gmail.com>
2017-02-18 16:55:52 -0800

    no message

--- fiveoneone/stop.py
+++ fiveoneone/stop.py
@@ -10,22 +10,23 @@ class Stop(Model):
 	@property
 	def name(self):
 	    return self._name
-	
+
 	@name.setter
 	def name(self, value):
 	    self._name = value
-	
+
 	@property
 	def code(self):
 	    return self._code
-	
+
 	@code.setter
 	def code(self, value):
 	    self._code = str(value)
 
 	def __str__(self):
-		return "{name} | {code} ".format(name=self.name, code=self.code)	
-	
+		return "{name} | {code} ".format(name=self.name, code=self.code)
+
+
 	def next_departures(self, route_code, direction=None):
 		tree = self.get("GetNextDeparturesByStopCode.aspx", dict(stopCode=self.code))
 		departure = Departure(route_code, self.code, direction)
@@ -42,3 +43,28 @@ class Stop(Model):
 									departure.times.append(int(d.text))
 
 		return departure
+
+	def all_departures(self):
+		tree = self.get("GetNextDeparturesByStopCode.aspx", dict(stopCode=self.code))
+		departures = []
+		for route in tree[0][0][0]:
+			departure = Departure(route.attrib["Code"], self.code, None)
+			for d in route[0]:
+				for stop in d[0]:
+					if stop.attrib["StopCode"] == self.code:
+						assert(stop[0].tag == "DepartureTimeList")
+						for d in stop[0]:
+							assert(d.tag == "DepartureTime")
+							departure.times.append(int(d.text))
+			departures.append(departure)
+
+		return departures
+
+	def load(self):
+		tree = self.get("GetNextDeparturesByStopCode.aspx", dict(stopCode=self.code))
+		for route in tree[0][0][0]:
+			for d in route[0]:
+				for stop in d[0]:
+					if stop.attrib["StopCode"] == self.code:
+						self.name = stop.attrib["name"]
+						break
