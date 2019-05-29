9bfc2b285872a4dec71243a90788fc8da1ae082c
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-05-29 09:53:34 -0700

    add long description

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ A python API to consume transit data from http://511.org.
 
 from setuptools import setup
 
-requirements = ("requests==2.22.0", )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='fiveoneone',
@@ -16,12 +17,14 @@ setup(
     author='Ramiro Berrelleza',
     author_email='rberrelleza@gmail.com',
     description='A python API to consume transit data from http://511.org.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/rberrelleza/511-transit',
     packages=['fiveoneone'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=requirements,
+    install_requires=['requests>=2.22'],
     entry_points={
     },
     classifiers=[
